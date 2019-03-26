0f18742714347f5eb9191d938c3b86a7541cc643
Rich Harris <richard.a.harris@gmail.com>
2019-03-26 18:18:55 -0400

    set up tests for later

---
+++ test/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Scroller from '../../Scroller.svelte';
+</script>
+
+<Scroller>
+	<div slot="background">
+		<!-- TODO -->
+	</div>
+
+	<div slot="foreground">
+		<!-- TODO -->
+	</div>
+</Scroller>
